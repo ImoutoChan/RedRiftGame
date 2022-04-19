17b67f239cd88bad9e67187bc63b33be61b2629b
ImoutoChan <kirirrin@gmail.com>
2022-04-19 17:49:21 +0400

    Add scope for GameLobbyRunnerHostedService

--- RedRiftGame/GameLobbyRunnerHostedService.cs
+++ RedRiftGame/GameLobbyRunnerHostedService.cs
@@ -5,11 +5,13 @@ namespace RedRiftGame;
 internal class GameLobbyRunnerHostedService : IHostedService
 {
     private readonly IGameLobbyRunner _gameLobbyRunner;
+    private readonly IServiceScope _scope;
     private readonly CancellationTokenSource _cts;
 
-    public GameLobbyRunnerHostedService(IGameLobbyRunner gameLobbyRunner)
+    public GameLobbyRunnerHostedService(IServiceProvider serviceProvider)
     {
-        _gameLobbyRunner = gameLobbyRunner;
+        _scope = serviceProvider.CreateScope();
+        _gameLobbyRunner = _scope.ServiceProvider.GetRequiredService<IGameLobbyRunner>();
         _cts = new CancellationTokenSource();
     }
 
@@ -24,7 +26,8 @@ internal class GameLobbyRunnerHostedService : IHostedService
     public Task StopAsync(CancellationToken cancellationToken)
     {
         _cts.Cancel();
-
+        _scope.Dispose();
+        
         return Task.CompletedTask;
     }
 }
