67537fedf04fe32113ad938987e893d28a88a1d5
ImoutoChan <kirirrin@gmail.com>
2022-04-19 23:36:07 +0400

    Improve thread safety of the GameLobby

--- RedRiftGame.Application/Services/GameLobby.cs
+++ RedRiftGame.Application/Services/GameLobby.cs
@@ -1,4 +1,5 @@
-﻿using NodaTime;
+﻿using System.Collections.Concurrent;
+using NodaTime;
 using RedRiftGame.Domain;
 
 namespace RedRiftGame.Application.Services;
@@ -6,23 +7,21 @@ namespace RedRiftGame.Application.Services;
 // singleton
 internal class GameLobby : IGameLobby
 {
-    private readonly List<Match> _currentMatches;
+    private readonly ConcurrentDictionary<Guid, Match> _currentMatches;
 
-    public GameLobby() => _currentMatches = new List<Match>();
+    public GameLobby() => _currentMatches = new ConcurrentDictionary<Guid, Match>();
 
     public void CreateMatch(Match match)
     {
-        if (_currentMatches.Any(x => x.Host.ConnectionId != match.Host.ConnectionId))
+        if (_currentMatches.Any(x => x.Value.Host.ConnectionId != match.Host.ConnectionId))
             throw new MatchHandlingException("Player already has room");
 
-        _currentMatches.Add(match);
+        _currentMatches.TryAdd(match.Id, match);
     }
 
     public Match JoinMatch(Guid id, Player guest)
     {
-        var match = _currentMatches.FirstOrDefault(x => x.Id == id);
-
-        if (match == null)
+        if (!_currentMatches.TryGetValue(id, out var match))
             throw new MatchHandlingException("Match can't be found");
 
         match.Join(guest);
@@ -32,23 +31,25 @@ internal class GameLobby : IGameLobby
 
     public void RunMatches(Instant now)
     {
-        var runningMatches = _currentMatches.Where(x => x.MatchState == MatchState.Running);
+        var runningMatches = _currentMatches.Where(x => x.Value.MatchState == MatchState.Running);
 
         foreach (var runningMatch in runningMatches) 
-            runningMatch.NextTurn(now);
+            runningMatch.Value.NextTurn(now);
     }
 
     public void InterruptMatch(string hostConnectionId)
         => _currentMatches
-            .FirstOrDefault(x => x.Host.ConnectionId == hostConnectionId && x.MatchState == MatchState.Created)
-            ?.Interrupt();
+            .FirstOrDefault(x => x.Value.Host.ConnectionId == hostConnectionId 
+                                 && x.Value.MatchState == MatchState.Created)
+            .Value?.Interrupt();
 
     public IReadOnlyCollection<Match> RemoveFinishedMatches()
     {
-        var finishedMatches = _currentMatches.Where(x => x.MatchState == MatchState.Finished).ToList();
-        var finishedMatchesIds = finishedMatches.Select(x => x.Id).ToHashSet();
-        _currentMatches.RemoveAll(x => finishedMatchesIds.Contains(x.Id));
+        var finishedMatches = _currentMatches.Where(x => x.Value.MatchState == MatchState.Finished).ToList();
 
-        return finishedMatches;
+        foreach (var finishedMatch in finishedMatches) 
+            _currentMatches.TryRemove(finishedMatch.Key, out _);
+
+        return finishedMatches.Select(x => x.Value).ToList();
     }
 }
