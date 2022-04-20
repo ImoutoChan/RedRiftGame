8348c70731f37c8f88ae2c89dbd4505126cffb7f
ImoutoChan <kirirrin@gmail.com>
2022-04-20 12:28:43 +0400

    Finishing touches

--- RedRiftGame/Program.cs
+++ RedRiftGame/Program.cs
@@ -1,19 +1,15 @@
+using RedRiftGame;
 using RedRiftGame.Application;
-using RedRiftGame.Application.Services;
 using RedRiftGame.DataAccess;
 using RedRiftGame.Hubs;
-using RedRiftGame.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 builder.Services
-    .AddTransient<IMatchClientReporter, SignalRMatchClientReporter>()
-    .AddHostedService<MatchRunnerHostedService>()
-    .AddHostedService<MatchReporterHostedService>()
     .AddApplication()
     .AddPersistence(configuration)
-    .AddSignalR(x => x.EnableDetailedErrors = true);
+    .AddHost();
 
 var app = builder.Build();
 app.MapHub<GameLobbyHub>("/GameLobby");
---
+++ RedRiftGame/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using RedRiftGame.Application.Services;
+using RedRiftGame.Services;
+
+namespace RedRiftGame;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHost(this IServiceCollection services)
+    {
+        services
+            .AddHostedService<MatchRunnerHostedService>()
+            .AddHostedService<MatchReporterHostedService>()
+            .AddTransient<IMatchClientReporter, SignalRMatchClientReporter>()
+            .AddSignalR(x => x.EnableDetailedErrors = true);
+
+        return services;
+    }
+}
